745d1aa91b39e0fdc3e70706aaffb8a7e8c2d82c
abrech <adrian.brechtken@gmail.com>
2024-11-15 13:49:01 +0100

    added (re)start function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from utils.chat import define_goal
+from utils.chat import define_goal, handle_user_input
 from utils.chat import guess
 
 load_dotenv()
@@ -18,10 +18,8 @@ st.title("💬 Chatbot")
 
 client = OpenAI(api_key=openai_api_key)
 
-
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -29,27 +27,5 @@ for msg in st.session_state.messages:
 if "goal" not in st.session_state:
     define_goal(client)
 
-#if prompt := st.chat_input():
- #   if not openai_api_key:
-  #      st.info("Please add your OpenAI API key to continue.")
-   #     st.stop()
-
-#prompt = st.chat_input("Type here...")
-#current_message =  st.session_state.messages.content
-
-
-
 client = OpenAI(api_key=openai_api_key)
-if prompt:=st.chat_input("Type here..."):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    if prompt.lower().startswith("guess:"):
-        guess(client, message =  prompt)
-    elif prompt:
-        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-        msg = response.choices[0].message.content
-        st.session_state.messages.append({"role": "assistant", "content": msg})
-        st.chat_message("assistant").write(msg)
-
-
-
+handle_user_input(client)
--- utils/chat.py
+++ utils/chat.py
@@ -1,18 +1,36 @@
 import streamlit as st
 from openai import OpenAI
+
+
 def append_and_write(role: str, message: str):
+    """
+    appends the given message and writes it to the chat
+    :param role: role of the message
+    :param message: message to be appended and written
+    :return:
+    """
     st.session_state.messages.append({"role": role, "content": message})
     st.chat_message(role).write(message)
 
-def define_goal(client:OpenAI):
+
+def define_goal(client: OpenAI):
+    """
+    defines a goal for the streamlit session by prompting ChatGPT
+    :param client: OpenAI client to use for prompts
+    """
     goal_prompt = {"role": "user",
                    "content": "Give one random noun for my guessing game. Your answer should only consist of that one word."}
     st.session_state.goal = client.chat.completions.create(model="gpt-3.5-turbo", messages=[goal_prompt]).choices[
         0].message.content
     append_and_write("assistant", "Next guess word is: " + st.session_state.goal)
 
-def guess(client:OpenAI, message:str):
-    """function that evaluates whether the guess for the goal is correct"""
+
+def guess(client: OpenAI, message: str):
+    """
+    function that evaluates whether the guess for the goal is correct
+    :param client: OpenAI client to use for prompts
+    :param message: user message
+    """
     return_message = ""
     if st.session_state.goal.lower() not in message.lower():
         return_message = "Not quite yet. Guess again or continue asking yes/no questions."
@@ -21,8 +39,38 @@ def guess(client:OpenAI, message:str):
     append_and_write("assistant", return_message)
     if st.session_state.goal.lower() in message.lower():
         st.balloons()
+        # FIXME das hier vllt eher wenn man den restart button drückt? und hier stattdessen "Wenn du nochmal spielen
+        # FIXME möchtest, drücke einfach den restart button"
         define_goal(client)
         append_and_write("assistant", "I've got a new word for you. You can just continue playing as before.")
 
 
+def start(client: OpenAI, clear: bool = True, intro_msg: str = ""):
+    """
+    starts a round of the guessing game
+    :param client: OpenAI client to use for prompts
+    :param clear: whether to delete previous messages
+    :param intro_msg: message that's sent at the start of the game (if provided. By default, no message is sent)
+    """
+    if clear:
+        st.session_state.messages = []
+    define_goal(client)
+    if intro_msg:
+        append_and_write("assistant", intro_msg)
 
+
+def handle_user_input(client: OpenAI):
+    """
+    handles user input
+    :param client: OpenAI client to use for prompts
+    """
+    if prompt := st.chat_input("Type here..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        if prompt.lower().startswith("guess:"):
+            guess(client, message=prompt)
+        elif prompt:
+            response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+            msg = response.choices[0].message.content
+            st.session_state.messages.append({"role": "assistant", "content": msg})
+            st.chat_message("assistant").write(msg)
