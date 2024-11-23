fdca684151e3f17fe93e06217f1c33f39a76c75d
noahs <nschade@uni-osnabrueck.,de>
2024-11-23 15:57:31 +0100

    Statistic repariert

--- stats.py
+++ stats.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.write("hi")
-i = 0
+i = 1
+
+
 for element in st.session_state.statistics:
     st.write("Game " + str(i) + ": ")
     st.write(element)
     i += 1
+
+num_guesses = []
+if st.session_state.statistics:
+    for element in st.session_state.statistics:
+        num_guesses.append(element.guesses)
+    data_frame = pd.DataFrame(num_guesses, columns= ["Numbers"])
+    st.bar_chart(data_frame)
