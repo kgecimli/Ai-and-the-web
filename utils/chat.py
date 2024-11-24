3ef6e7650fa03c468d0b551b4bcc78bf08c07398
abrech <adrian.brechtken@gmail.com>
2024-11-24 18:53:53 +0100

    almost done

--- utils/chat.py
+++ utils/chat.py
@@ -1,81 +1,92 @@
+import random
+
 import streamlit as st
 from nltk.corpus import wordnet as wn
 from streamlit import session_state
 
-from utils.Message import Message
 from utils.Statistics import Statistics
-from utils.constants import INTRO_MSG, ASSISTANT, USER, GPT_VERSION
-
-
-def append_message(role: str, message: str, hidden: bool = False, write: bool = False):
-    """
-    appends the given message and writes it to the chat
-    :param write: whether to instantly write the message
-    :param role: role of the message
-    :param message: message to be appended and written
-    :param hidden: whether the message is hidden or not
-    """
-
-    st.session_state.messages.append({"role": role, "content": message, "hidden": hidden})
-    if not hidden and write:
-        write_message(role, message)
+from utils.constants import INTRO_MSG, ASSISTANT, USER, GPT_VERSION, BACKUP_GOAL_WORDS, DEBUG
 
 
 def write_message(role, message):
     st.chat_message(role).write(message)
 
 
-def define_goal():
+def define_goal(depth=0):
     """
     defines a goal for the streamlit session by prompting ChatGPT
+    :parameter depth: recursion depth as a failsafe
     """
-    prompt = "Give one random noun for my guessing game. Your answer should only consist of that one word. I really need a one word answer, any answer with more than one word will destroy my code."
+    prompt = ("Give one random noun for my guessing game. Your answer should only consist of that one word. "
+              "I really need a one word answer, any answer with more than one word will destroy my code.")
     already_used = [goal for goal in st.session_state.goals]
     if already_used:
         prompt += "Do not use any of the following words: " + ", ".join(already_used)
-    st.session_state.goal = send_prompt(prompt)
+    new_goal = send_prompt(prompt)
 
     # check whether ChatGPTs goal word really only consists of one word, if yes append it, else redefine the goal word
-    if len(st.session_state.goal.split()) == 1:
+    if len(new_goal.split()) == 1:
         # Remove all non-letter characters
-        only_letters = ''.join(char for char in st.session_state.goal if char.isalpha())
+        only_letters = ''.join(char for char in new_goal if char.isalpha())
         st.session_state.goals.append(only_letters)
-        # debug to see the goal
-        if st.session_state.debug:
-            write_message(ASSISTANT, "Next guess word is: " + st.session_state.goal)
+        st.session_state.goal = only_letters
+
+    elif depth >= 5:
+        # in case ChatGPT goes crazy and doesnt give us a proper goal word
+        new_goal = random.choice(BACKUP_GOAL_WORDS)
+        st.session_state.goals.append(new_goal)
+        st.session_state.goal = new_goal
+        send_prompt("I decided on a goal word by myself. In the following I will ask questions trying to guess "
+                    "the word.")
     else:
-        define_goal()
+        # trying again
+        depth += 1
+        define_goal(depth)
+
+    # debug to see the goal
+    if st.session_state.debug:
+        write_message(ASSISTANT, "Next guess word is: " + st.session_state.goal)
 
 
 def handle_user_input():
     """
     handles user input
     """
-    if user_msg := st.chat_input("Type here..."):
-        append_message(USER, user_msg, write=True)
-        # we saved the original user_msg, so we can modify it for following checks
-        user_msg = user_msg.lower()
-        if user_msg.startswith("guess:"):
-            st.session_state.statistics[-1].guesses += 1
-            # splits the prompt and excludes the first word (guess:) and any spaces, such that only the actual guess is passed to the guess function
-            print(user_msg.split("guess:"))
-            evaluate_guess(guess=user_msg.split("guess:")[1].strip())
-        elif user_msg:
-            st.session_state.statistics[-1].questions += 1
-            # make sure chatgpt knows what to do
-            append_text = (f". As a reminder, the goal word is {st.session_state.goal} and you should only ever "
-                           f"respond with 'Yes' or 'No'.")
-            # copy messages by value so we can modify the last user message for chatgpt without displaying the change
-            prompt_msgs = st.session_state.messages[:]
-            prompt_msgs[-1] = {"role": prompt_msgs[-1]["role"], "content": prompt_msgs[-1]["content"] + append_text}
-            response = st.session_state.client.chat.completions.create(model="gpt-3.5-turbo", messages=prompt_msgs)
-            msg = response.choices[0].message.content
-            counter = 0  # counter for max amount of iterations
-            while not yes_no_function(msg) and counter <= 5:
-                msg = correct_response(user_msg, msg)
-                counter += 1
+    user_msg = st.chat_input("Type here...")
+    # if we dont get a user_msg we exit the function
+    if not user_msg:
+        return
 
-            append_message(ASSISTANT, msg, write=True)
+    append_msg(USER, user_msg)
+    write_message(USER, user_msg)
+    # we saved the original user_msg, so we can modify it for following checks
+    user_msg = user_msg.lower()
+    if user_msg.startswith("guess:"):
+        st.session_state.statistics[-1].guesses += 1
+
+        # splits the prompt and excludes the first word (guess:) and any spaces, such that only the actual guess
+        # is passed to the guess function
+        if st.session_state.debug:
+            print(user_msg.split("guess:"))
+        evaluate_guess(guess=user_msg.split("guess:")[1].strip())
+
+    else:
+        st.session_state.statistics[-1].questions += 1
+        # make sure chatgpt knows what to do
+        append_text = (f". As a reminder, the goal word is {st.session_state.goal} and you should only ever "
+                       f"respond with 'Yes' or 'No'.")
+        # copy messages by value so we can modify the last user message for chatgpt without displaying the change
+        prompt_msgs = st.session_state.messages[:]
+        prompt_msgs[-1] = {"role": prompt_msgs[-1]["role"], "content": prompt_msgs[-1]["content"] + append_text}
+        response = st.session_state.client.chat.completions.create(model="gpt-3.5-turbo", messages=prompt_msgs)
+        msg = response.choices[0].message.content
+        counter = 0  # counter for max amount of iterations
+        while not yes_no_function(msg) and counter <= 5:
+            msg = correct_response(user_msg, msg)
+            counter += 1
+
+        append_msg(ASSISTANT, msg)
+        write_message(ASSISTANT, msg)
 
 
 def give_hint():
@@ -133,7 +144,7 @@ def init_session_variables():
         st.session_state.statistics = []
     # debug flag for e.g. showing the goal word in the chat
     if "debug" not in st.session_state:
-        st.session_state.debug = False
+        st.session_state.debug = DEBUG
     # all prompts and responses sent and received from ChatGPT
     if "prompts" not in st.session_state:
         st.session_state.prompts = []
--- utils/constants.py
+++ utils/constants.py
@@ -2,3 +2,5 @@ INTRO_MSG = "I've got a new word for you. You can just continue playing as befor
 ASSISTANT = "assistant"
 USER = "user"
 GPT_VERSION = "gpt-4o-mini" # gpt-3.5-turbo
+BACKUP_GOAL_WORDS = ["car", "fish", "orange", "christmas", "water", "candle", "guitar", "controller", "wardrobe"]
+DEBUG = True
