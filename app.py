d606369129119d2ee1e0c6ef38aaa8c0338671b8
abrech <adrian.brechtken@gmail.com>
2024-11-26 16:48:53 +0100

    downloading wordnet

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-import os
-
+import nltk
 import streamlit as st
-from dotenv import load_dotenv  # used to load the OpenAI key from the .env file
 from openai import OpenAI
 
 from utils.chat import init_session_variables
 
+nltk.download("wordnet")
+
 # setup
 if "loaded" not in st.session_state or not st.session_state.loaded:  # st.session_state ist ein dictionary
     # loading the OpenAI key and creating a client
-    load_dotenv()
-    openai_api_key = os.getenv('OPENAI_KEY')
+    openai_api_key = st.secrets.get("OPENAI_KEY")
     client = OpenAI(api_key=openai_api_key)
 
     # initialising all session variables once so we don't have to check if they exist every time
@@ -24,5 +23,5 @@ game_page = st.Page("game.py", title="Guessing game", icon=":material/sports_esp
 stats_page = st.Page("stats.py", title="Statistics", icon=":material/bar_chart:")
 
 pg = st.navigation([game_page, stats_page])
-st.set_page_config(page_title="Guessing GPT", page_icon=":material/sports_esports:")
+st.set_page_config(page_title="Guessing GPT", page_icon=":material/sports_esports:", layout="wide")
 pg.run()
