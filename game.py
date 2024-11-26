dbafea0fa19c90b8478065a93125486b41362585
noahs <nschade@uni-osnabrueck.,de>
2024-11-26 17:26:47 +0100

    korays schlimme fehler ausgebügelt

--- game.py
+++ game.py
@@ -7,9 +7,9 @@ from utils.ui import sidebar
 
 # ui building
 with st.expander("Rules"):
-    st.write("Welcome to the guessing game. I randomly chose a word you should guess now. You can either "
-                    "ask me Yes/No questions or guess by typing 'Guess: ' followed by your guess. If you need a hint,"
-             "want to give up or restart the game, use the buttons on the sidebar.")
+    st.write("In this guessing game, I will provide you with a random noun. You can ask me as many yes/no questions"
+             "as you want. If you think you got the word, you can guess by typing 'Guess:' followed by your guess. "
+             "If you need a hint, want to give up or restart the game, use the buttons on the sidebar.")
 
 st.title("💬 Chatbot")
 write_messages()
